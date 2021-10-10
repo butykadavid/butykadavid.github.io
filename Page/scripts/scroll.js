552303522fd41599043e0b1840c1cda171915fb3
dawson0810 <butyka.david@gmail.com>
2021-10-10 18:12:53 +0200

    reconstructions

---
+++ Page/scripts/scroll.js
@@ -0,0 +1,9 @@
+var scrollCounter = 0;
+
+window.onscroll = function(e) {
+    // print "false" if direction is down and "true" if up
+    console.log(this.scrollY);
+    if(this.scrollY > 500 && this.scrollY < 1000){
+        
+    }
+  }
