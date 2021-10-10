49219f1f7ab555ed1757b08a2ccef0398bf34737
dawson0810 <butyka.david@gmail.com>
2021-10-10 21:00:35 +0200

    valami

--- Page/scripts/scroll.js
+++
@@ -1,9 +0,0 @@
-var scrollCounter = 0;
-
-window.onscroll = function(e) {
-    // print "false" if direction is down and "true" if up
-    console.log(this.scrollY);
-    if(this.scrollY > 500 && this.scrollY < 1000){
-        
-    }
-  }
