c7d8c0c87541ebff7be0bec583d5c2cdaad23eaa
dawson0810 <butyka.david@gmail.com>
2021-10-13 18:20:03 +0200

    theme complete

--- Page/scripts/changeTheme.js
+++ Page/scripts/changeTheme.js
@@ -1,7 +1,7 @@
 const OriginalTheme = function(){
 
     document.body.style.setProperty('--primary-color',"rgb(92, 118, 148)")
-    document.body.style.setProperty('--secondary-color',"rgb(253, 194, 66)")
+    document.body.style.setProperty('--secondary-color',"rgb(251, 216, 23)")
     document.body.style.setProperty('--bg-color',"rgba(56, 84, 116, 0.555)")
 }
 
@@ -21,21 +21,21 @@ const CyberpunkTheme = function(){
 
 const IntergalacticCrimsonTheme = function(){
 
-    document.body.style.setProperty('--primary-color',"rgb(51,184,255)")
-    document.body.style.setProperty('--secondary-color',"rgb(37,37,37)")
-    document.body.style.setProperty('--bg-color',"rgba(255,255,255)")
+    document.body.style.setProperty('--primary-color',"#41053F")
+    document.body.style.setProperty('--secondary-color',"#EACAE9")
+    document.body.style.setProperty('--bg-color',"crimson")
 }
 
 const RelaxedMountainsTheme = function(){
 
-    document.body.style.setProperty('--primary-color',"rgb(51,184,255)")
-    document.body.style.setProperty('--secondary-color',"rgb(37,37,37)")
-    document.body.style.setProperty('--bg-color',"rgba(255,255,255)")
+    document.body.style.setProperty('--primary-color',"rgb(193,172,191)")
+    document.body.style.setProperty('--secondary-color',"rgb(94,84,101)")
+    document.body.style.setProperty('--bg-color',"rgba(255,243,234)")
 }
 
 const ColorfulFallTheme = function(){
 
-    document.body.style.setProperty('--primary-color',"rgb(51,184,255)")
-    document.body.style.setProperty('--secondary-color',"rgb(37,37,37)")
-    document.body.style.setProperty('--bg-color',"rgba(255,255,255)")
+    document.body.style.setProperty('--primary-color',"rgb(144,88,30)")
+    document.body.style.setProperty('--secondary-color',"rgb(80,142,163)")
+    document.body.style.setProperty('--bg-color',"rgba(236,208,178)")
 }
