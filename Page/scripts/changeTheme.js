f53809aed3a483df423d7a6cc070f10247769e4e
dawson0810 <butyka.david@gmail.com>
2021-10-13 10:31:54 +0200

    themes

---
+++ Page/scripts/changeTheme.js
@@ -0,0 +1,13 @@
+const OriginalTheme = function(){
+
+    document.body.style.setProperty('--primary-color',"rgb(92, 118, 148)")
+    document.body.style.setProperty('--secondary-color',"rgb(253, 194, 66)")
+    document.body.style.setProperty('--bg-color',"rgba(56, 84, 116, 0.555)")
+}
+
+const LightTheme = function(){
+
+    document.body.style.setProperty('--primary-color',"rgb(51,184,255)")
+    document.body.style.setProperty('--secondary-color',"rgb(37,37,37)")
+    document.body.style.setProperty('--bg-color',"rgba(255,255,255)")
+}
